9163c37c606b98196fd4a0d75f671242aec22a0d
haixiangyan <haixiang6123@gmail.com>
2021-07-18 22:13:36 +0800

    feat: 添加图片

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,17 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Image(
+              image: AssetImage(
+                'assets/images/logo.png'
+              )
+            )
+          )
+        )
+      )
     );
   }
 }
