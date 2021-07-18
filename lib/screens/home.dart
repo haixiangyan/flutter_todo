727d87c3481e9f6ae2fba2413a89bd6d0583959e
haixiangyan <haixiang6123@gmail.com>
2021-07-18 22:57:01 +0800

    feat: 添加 TaskCard

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo/widgets/TaskCard.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -11,15 +12,41 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: SafeArea(
         child: Container(
+          width: double.infinity,
           padding: EdgeInsets.symmetric(
             horizontal: 24.0,
             vertical: 32.0,
           ),
-          child: Column(
+          color: Color(0xFFF6F6F6),
+          child: Stack(
             children: [
-              Image(image: AssetImage('assets/images/logo.png')
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image(image: AssetImage('assets/images/logo.png')),
+                  Container(
+                      margin: EdgeInsets.only(
+                        bottom: 32.0,
+                      )
+                  ),
+                  TaskCardWidget(title: 'Get Stated', desc: 'Hello',),
+                  TaskCardWidget(),
+                ]
+              ),
+              Positioned(
+                bottom: 0.0,
+                right: 0.0,
+                child: Container(
+                  width: 60.0,
+                  height: 60.0,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF7349FE),
+                    borderRadius: BorderRadius.circular(50.0)
+                  ),
+                  child: Image(image: AssetImage('assets/images/add_icon.png'))
+                )
               )
-            ]
+            ],
           )
         )
       )
---
+++ lib/widgets/TaskCard.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class TaskCardWidget extends StatelessWidget {
+  final String? title;
+  final String? desc;
+
+  TaskCardWidget({this.title, this.desc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(
+        vertical: 32.0,
+        horizontal: 24.0,
+      ),
+      margin: EdgeInsets.only(
+        bottom: 20.0
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20.0)
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title ?? '(Unnamed Task)',
+            style: TextStyle(
+              color: Color(0xFF211551),
+              fontSize: 22.0,
+              fontWeight: FontWeight.bold
+            )
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              top: 16.0
+            ),
+            child: Text(
+              desc ?? 'Enter your description here.',
+              style: TextStyle(
+                color: Color(0xFF86829D),
+                fontSize: 16.0,
+                height: 1.5,
+              ),
+            ),
+          )
+        ]
+      )
+    );
+  }
+}
